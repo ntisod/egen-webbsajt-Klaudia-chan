b0d5f22e5c8605a1717d9b2f92616955f0de7878
Klaudia-chan <ja.klaudynka123@gmail.com>
2020-03-16 10:52:42 +0100

    exempel om java script
    är inte klar än med 4 uppgift

---
+++ javascript-exempel.js
@@ -0,0 +1,11 @@
+
+
+function authorDialog(){
+    alert('Klaudia K skapade den här sidan år 2020')
+}
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Klaudia K";
+}
+function Funktion(){
+    document.getElementById('matbild').src = kebab.jpg;
+}
