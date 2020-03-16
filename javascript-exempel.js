9d29761f1a38e6bdb6bbc9505e3596cba1dba15a
Klaudia-chan <ja.klaudynka123@gmail.com>
2020-03-16 12:23:13 +0100

    slut version

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -4,8 +4,14 @@ function authorDialog(){
     alert('Klaudia K skapade den här sidan år 2020')
 }
 function myFunction(){
-    document.getElementById("demo").innerHTML = "Klaudia K";
+    document.getElementById("vanligText").innerHTML = "Klaudia K";
 }
-function Funktion(){
-    document.getElementById('matbild').src = "kebab.jpg";
+function mat(sw){
+    var pic;
+    if (sw == 0){
+        pic = "sushi.jpg"
+    } else {
+        pic = "kebab.jpg"
+    }
+    document.getElementById('matbild').src = pic;
 }
